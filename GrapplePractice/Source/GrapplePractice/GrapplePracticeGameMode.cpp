18d47746cf7f573d3820f135dedde981fa108f2c
wizardofsalem <caltriestocode@gmail.com>
2026-09-23 01:15:40 +0100

    add HUDClass to game mode

--- GrapplePractice/Source/GrapplePractice/GrapplePracticeGameMode.cpp
+++ GrapplePractice/Source/GrapplePractice/GrapplePracticeGameMode.cpp
@@ -1,8 +1,9 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "GrapplePracticeGameMode.h"
+#include "Public/GrappleHud.h"
 
 AGrapplePracticeGameMode::AGrapplePracticeGameMode()
 {
-	// stub
+	HUDClass = AGrappleHud::StaticClass();
 }
